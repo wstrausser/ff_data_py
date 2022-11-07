ee45006e5a28723f95a7954c4b7501ae24b782e6
William E. Strausser <william.e.strausser@gmail.com>
2022-11-06 22:21:21 -0800

    Moved comment to module-level docstring in tests/__init__.py

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,10 @@
+"""
+Appending project directory to path to allow import of project code in tests
+"""
+
 import os
 import sys
 
-# Appending project directory to path to allow import of project code in tests
 FILE_DIRECTORY = os.path.dirname(__file__)
 PROJECT_DIRECTORY = os.path.dirname(FILE_DIRECTORY)
 sys.path.append(PROJECT_DIRECTORY)
